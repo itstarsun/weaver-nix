34598503f2f4a6b1f04523826e820900a06f0cf6
itstarsun <itstarsun@proton.me>
2023-03-05 07:05:40 +0000

    add nixos module

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./flake-module.nix
+        ./nixos-module.nix
       ];
 
       systems = [
@@ -18,9 +19,41 @@
 
       flake.flakeModule = import ./flake-module.nix;
 
-      perSystem = { config, ... }: {
+      perSystem = { config, pkgs, ... }: {
         packages.weaver = config.weaver.package;
         packages.weaver-gke = config.weaver.gke.package;
+
+        checks.weaver = pkgs.nixosTest {
+          name = "weaver";
+
+          nodes.machine = { pkgs, ... }: {
+            imports = [
+              inputs.self.nixosModules.weaver
+            ];
+
+            services.weaver = {
+              enable = true;
+
+              dashboard.enable = true;
+
+              deployments = {
+                invalid = {
+                  binary = "${pkgs.hello}/bin/hello";
+                };
+              };
+            };
+          };
+
+          testScript = ''
+            machine.start()
+            machine.wait_for_unit("default.target")
+
+            machine.wait_for_unit("weaver-dashboard.service")
+            machine.succeed("curl http://localhost:27333")
+
+            # TODO: Test invalid deployment?
+          '';
+        };
       };
     };
 }
---
+++ nixos-module.nix
@@ -0,0 +1,168 @@
+{ moduleWithSystem, ... }:
+
+{
+  flake = {
+    nixosModules.weaver = moduleWithSystem (perSystem @ { config }: { config, lib, pkgs, ... }:
+      with lib;
+
+      let
+        cfg = config.services.weaver;
+
+        mkDeploymentService = name: deployment:
+          let
+            configfile = pkgs.writeText "weaver.toml" ''
+              [serviceweaver]
+              name = "${name}"
+              binary = "${deployment.binary}"
+            '';
+          in
+          {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+            serviceConfig = {
+              ExecStart = "${cfg.package}/bin/weaver multi deploy ${configfile}";
+              Restart = "always";
+              RestartSec = "10s";
+              User = cfg.user;
+              Group = cfg.group;
+            };
+          };
+      in
+
+      {
+        options = {
+          services.weaver = {
+            enable = mkEnableOption "weaver";
+
+            package = mkOption {
+              type = types.package;
+              default = perSystem.config.weaver.package;
+            };
+
+            user = mkOption {
+              type = types.str;
+              default = "weaver";
+            };
+
+            group = mkOption {
+              type = types.str;
+              default = "weaver";
+            };
+
+            dashboard = {
+              enable = mkEnableOption "weaver dashboard";
+
+              host = mkOption {
+                type = types.str;
+                default = "localhost";
+              };
+
+              port = mkOption {
+                type = types.port;
+                default = 27333;
+              };
+            };
+
+            deployments = mkOption {
+              type = types.attrsOf
+                (types.submodule {
+                  options = {
+                    binary = mkOption {
+                      type = types.str;
+                      description = lib.mdDoc ''
+                        Compiled Service Weaver application. The binary path, if not absolute,
+                        should be relative to the directory that contains the config file.
+                      '';
+                    };
+
+                    args = mkOption {
+                      type = types.listOf types.str;
+                      default = [ ];
+                      example = [ "these" "are" "command" "line" "arguments" ];
+                      description = lib.mdDoc ''
+                        Command line arguments passed to the binary.
+                      '';
+                    };
+
+                    env = mkOption {
+                      type = types.attrsOf types.str;
+                      default = { };
+                      example = {
+                        PUT = "your";
+                        ENV = "vars";
+                        HERE = "";
+                      };
+                      description = lib.mdDoc ''
+                        Environment variables that are set before the binary executes.
+                      '';
+                    };
+
+                    colocate = mkOption {
+                      type = types.listOf (types.listOf types.str);
+                      default = [ ];
+                      example = [
+                        [ "main/Rock" "main/Paper" "main/Scissors" ]
+                        [ "github.com/example/sandy/PeanutButter" "github.com/example/sandy/Jelly" ]
+                      ];
+                      description = lib.mdDoc ''
+                        List of colocation groups. When two components in the same colocation group are deployed,
+                        they are deployed in the same OS process, where all method calls between them are performed
+                        as regular Go method calls. To avoid ambiguity, components must be prefixed by their full package path
+                        (e.g., github.com/example/sandy/). Note that the full package path of the main package in an executable is main.
+                      '';
+                    };
+
+                    rollout = mkOption {
+                      type = types.nullOr types.str;
+                      default = null;
+                      example = "1m";
+                      description = lib.mdDoc ''
+                        How long it will take to roll out a new version of the application.
+                        See the GKE Deployments section for more information on rollouts.
+                      '';
+                    };
+                  };
+                });
+              default = { };
+            };
+          };
+        };
+
+        config = mkIf cfg.enable (mkMerge [
+          {
+            users.users.weaver = mkIf (cfg.user == "weaver") {
+              group = cfg.group;
+              home = "/var/lib/weaver";
+              createHome = true;
+              isSystemUser = true;
+            };
+
+            users.groups.weaver = mkIf (cfg.group == "weaver") { };
+          }
+
+          (mkIf cfg.dashboard.enable {
+            systemd.services.weaver-dashboard = {
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              serviceConfig = {
+                ExecStart = "${cfg.package}/bin/weaver multi dashboard --host ${cfg.dashboard.host} --port ${toString cfg.dashboard.port}";
+                Restart = "always";
+                RestartSec = "10s";
+                User = cfg.user;
+                Group = cfg.group;
+              };
+            };
+          })
+
+          {
+            systemd.services = mapAttrs'
+              (name: deployment:
+                nameValuePair "weaver-deployment-${name}" (mkDeploymentService name deployment)
+              )
+              cfg.deployments;
+          }
+        ]);
+      }
+    );
+  };
+}
